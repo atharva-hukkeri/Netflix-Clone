ee369c8667f0cab4edd936199afa88d401f87da7
Atharva Hukkeri <92224529+atharva-hukkeri@users.noreply.github.com>
2022-01-29 20:57:35 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import Row from './Row';
+import requests from './requests';
+import Banner from './Banner';
+import Nav from './Nav';
+
+function App() {
+  return (
+    <div className="app">
+      <Nav />
+      <Banner />
+      <Row title="NETFLIX ORIGINAL" 
+      fetchUrl={requests.fetchNetflixOriginals} 
+      isLargeRow
+      />
+      <Row title="Popular on Netflix" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+    </div>
+  );
+}
+
+export default App;
